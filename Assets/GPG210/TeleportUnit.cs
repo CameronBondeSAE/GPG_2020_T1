d156628c4d0adfa605eced17f8babb8267733bd7
Octavia <1016089@student.sae.edu.au>
2020-02-06 15:33:54 +1100

    Teleport Unit
    TU prefab & scripts (Unit & Editor)

---
+++ Assets/GPG210/TeleportUnit.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportUnit : MonoBehaviour
+{
+    public enum States
+    {
+        Walk,
+        Sprint,
+        Teleport,
+        Attack
+    }
+
+    public States currentState;
+    
+    public void Walking()
+    {
+        Debug.Log("Walking");
+    }
+    
+    public void Sprinting()
+    {
+        Debug.Log("Sprinting");
+    }
+
+    
+    public void Teleporting()
+    {
+        Debug.Log("Teleporting");
+    }
+
+    public void Attacking()
+    {
+        Debug.Log("Attacking");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case States.Walk:
+                break;
+            case States.Sprint:
+                break;
+            case States.Teleport:
+                break;
+            case States.Attack:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
