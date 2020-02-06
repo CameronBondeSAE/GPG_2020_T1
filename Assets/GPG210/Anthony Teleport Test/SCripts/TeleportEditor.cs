0d8e4af8d7f76a4c08741594bd33a74c9d673cde
ayounes4 <1016744@student.sae.edu.au>
2020-02-06 15:30:24 +1100

    GPG210 Anthony Teleport Player prefab added

---
+++ Assets/GPG210/Anthony Teleport Test/SCripts/TeleportEditor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportEditor : MonoBehaviour
+{
+    public bool isTeleporting = true;
+
+
+    public void Teleporting()
+    {
+        Debug.Log("Teleporting Activated");
+    }
+}
---
+++ Assets/GPG210/Anthony Teleport Test/SCripts/TeleportEditorGUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+[CustomEditor(typeof(TeleportEditor))]
+public class TeleportEditorGUI : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        TeleportEditor myScript = (TeleportEditor) target;
+
+        if (GUILayout.Button("Teleporting"))
+        {
+            myScript.Teleporting();
+        }
+    }
+
+   
+}
