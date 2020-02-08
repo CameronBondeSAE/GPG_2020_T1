a76ec79fd42f8808664d138ad090ce0cc5ec8102
Niall <1013734@student.sae.edu.au>
2020-02-08 18:32:43 +1100

    RankUpUnit0208 Update
    random wandering,

--- Assets/GPG210/NIall-1013734/Scripts/StatemachMinion.cs
+++ Assets/GPG210/NIall-1013734/Scripts/StatemachMinion.cs
@@ -3,13 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEditor;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
+using Random = UnityEngine.Random;
 
 public class StatemachMinion : MonoBehaviour
 {
-public enum States
+    public float wanderTime;
+    public float wanderSpeed;
+    public UnitLevelUp unitlvlup;
+    public enum States
+
 
 {
     Moving,
@@ -19,22 +23,54 @@ public enum States
 
 public States currentState;
 
+void awake()
+{
+   unitlvlup =  GetComponent<UnitLevelUp>();
+}
+
 
 void Update()
 {
     switch (currentState)
     {
         case States.Moving:
-          //  print("Unit is Moving");
+           print("Unit is Moving");
+           // random wandering if state is set to Moving.
+          if (wanderTime > 0)
+          {
+              transform.Translate(Vector3.forward * wanderSpeed);
+              wanderTime -= Time.deltaTime;
+          }
+          else
+          {
+              wanderTime = Random.Range(0.2f, 0.6f);
+              wanderSpeed = Random.Range(0f, 0.3f);
+              Wander();
+          }
+
+          
+           if (unitlvlup.Kills <= 3)
+           {
+             // increase Unit speed/attack by unitLevelup Kills divided by 6.
+           }
             break;
         case States.Attacking:
          //   print("Unit is Attacking");
             break;
-        case States.Dead:
-          //  print("Unit is Dead");
+        case States.Dead: 
+            // Destroys unit if State is set to Dead.
+            print("Unit is Dead");
+
+          Destroy(gameObject);
             break;
         default:
             throw new ArgumentOutOfRangeException();
     }
+}
+
+// picks random direction to wander in every time wanderTime = 0
+    void Wander()
+{
+    transform.eulerAngles = new Vector3(0, Random.Range (0, 180), 0);
 }
 }
