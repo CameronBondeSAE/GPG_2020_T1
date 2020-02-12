6b6dbf2138c52a5704d34615ddb697d02f6a11ac
Luca <lucatannler@hotmail.com>
2020-02-12 16:57:39 +1100

    Ability System Test (Ignore it!)
    Did some brainstorming and played around with making a ability system. not advanced at all - IGNORE!

---
+++ Assets/GPG220/Luca/Scripts/Abilities/Ability.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GPG220.Luca.Scripts.Abilities;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(fileName = "New Ability", menuName = "Game/Ability")]
+public class Ability : ScriptableObject
+{
+    public string abilityName = "";
+    public string abilityDescription = "";
+    public Sprite abilityImg;
+
+    public List<ActionBase> instantSelfActions;
+    public List<ActionBase> instantTargetActions;
+    
+    // TODO add requirements
+    
+}
---
+++ Assets/GPG220/Luca/Scripts/Abilities/UnitAbilityHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using GPG220.Luca.Scripts.Unit;
+using UnityEngine;
+
+namespace GPG220.Luca.Scripts.Abilities
+{
+    public class UnitAbilityHandler : MonoBehaviour
+    {
+        public UnitBase unitBase;
+
+        #region Events
+
+        public delegate void OnAbilityExecutedDel(UnitBase unitBase, Ability ability);
+
+        public event OnAbilityExecutedDel OnAbilityExecuted;
+
+        #endregion
+        
+        private void Start()
+        {
+            if (unitBase == null)
+                unitBase = GetComponent<UnitBase>();
+        }
+
+        public bool ExecuteAbility(Ability ability)
+        {
+            if(!CheckAbilityRequirements(ability))
+                return false;
+            
+            foreach (var instantAction in ability.instantSelfActions)
+            {
+                instantAction.ExecuteAction(unitBase);
+            }
+            
+            OnAbilityExecuted?.Invoke(unitBase, ability);
+            return true;
+        }
+
+        public bool CheckAbilityRequirements(Ability ability)
+        {
+            if (ability == null || ability.instantSelfActions == null)
+                return false;
+            
+            // TODO
+            return true;
+        }
+    }
+}
