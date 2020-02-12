3ecd9adf7a88bd5365b52b34c168b0e09534bdd2
Anthony <1016744@student.sae.edu.au>
2020-02-12 16:12:57 +1100

    Teleport Unit/Mage
    moving stuff into the main thing

---
+++ Assets/GPG210/Anthony Teleport Test/SCripts/FindClosest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class FindClosest : MonoBehaviour
+{
+    
+    void Update()
+    {
+        FindClosestEnemy();
+    }
+
+    void FindClosestEnemy()
+    {
+        float distanceToClosestEnemy = Mathf.Infinity;
+        Enemy closestEnemy = null;
+        Enemy[] allEnemies = GameObject.FindObjectsOfType<Enemy>();
+
+        foreach (Enemy currentEnemy in allEnemies)
+        {
+            float distanceToEnemy = (currentEnemy.transform.position - this.transform.position).sqrMagnitude;
+            if (distanceToEnemy < distanceToClosestEnemy)
+            {
+                distanceToClosestEnemy = distanceToEnemy;
+                closestEnemy = currentEnemy;
+            }
+        }
+         Debug.DrawLine(this.transform.position,closestEnemy.transform.position);
+         
+    }
+}
