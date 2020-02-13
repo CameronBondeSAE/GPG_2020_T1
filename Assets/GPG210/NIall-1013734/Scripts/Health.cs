984d50f15a5916f2e893f50564e84966b325d132
Niall <1013734@student.sae.edu.au>
2020-02-13 14:50:02 +1100

    health fix

--- Assets/GPG210/NIall-1013734/Scripts/Health.cs
+++ Assets/GPG210/NIall-1013734/Scripts/Health.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
     public class Health : MonoBehaviour
     {
@@ -8,10 +10,35 @@ using UnityEngine;
         public int startingHealth;
         public int currentHealth;
 
+        private UnityEvent deathEvent;
 
         public void Awake()
         {
             currentHealth = startingHealth;
             
+        }
+
+
+        public void Start()
+        {
+            if (deathEvent == null)
+                deathEvent = new UnityEvent();
+            
+            deathEvent.AddListener(Death);
+        }
+
+        private void Death()
+        {
+            Debug.Log("Death");
+        }
+
+
+        private void Update()
+        {
+            if (currentHealth <= 0 && deathEvent != null)
+            {
+                deathEvent.Invoke();
+            }
+
         }
     }
