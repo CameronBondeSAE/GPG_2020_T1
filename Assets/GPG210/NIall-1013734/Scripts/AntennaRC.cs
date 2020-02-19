e3d0aafe2d5718630b70497f60ff93d899a49dd5
Niall <1013734@student.sae.edu.au>
2020-02-19 16:52:17 +1100

    rankupunit healthsystem readd
    + raycast start for avoidance wandering

---
+++ Assets/GPG210/NIall-1013734/Scripts/AntennaRC.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntennaRC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics.Raycast(transform.position, Vector3.forward, 10f);
+        
+        
+    }
+}
