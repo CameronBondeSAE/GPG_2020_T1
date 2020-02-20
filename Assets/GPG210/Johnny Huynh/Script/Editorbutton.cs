08a4ecc2c4e1a77b2bc1528b2011a286b6115c8d
Unknown <unknown@example.com>
2020-02-20 16:59:43 +1100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/GPG210/Johnny Huynh/Script/Editorbutton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Editorbutton : MonoBehaviour
+{
+    public int things;
+    public string stuff;
+
+    public void Attack()
+    {
+        Debug.Log("Attacking");
+    }
+
+    public void Charging()
+    {
+        Debug.Log("Charging");
+    }
+    
+    
+}
---
+++ Assets/GPG210/Johnny Huynh/Script/NPCSpearmen.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using GPG220.Blaide_Fedorowytsch.Scripts;
+using UnityEditor.Experimental.TerrainAPI;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class NPCSpearmen : TestUnitBase
+{
+    public float cooldown;
+    
+    public enum States
+    {
+        Attacking,
+        Hiding,
+        Charging
+    }
+
+    public States currentStates;
+    // Start is called before the first frame update
+    
+    // Update is called once per frame
+    private void Update()
+    {
+        cooldown += Time.deltaTime;
+        if (cooldown > 5)
+        {
+            Debug.Log("Working");
+            
+        }
+    }
+
+    public override void OnSelected()
+    {
+        base.OnSelected();
+        Debug.Log("Spearmen selected");
+    }
+
+    public override void OnExecuteAction(Vector3 worldPosition, GameObject g)
+    {
+        base.OnExecuteAction(worldPosition, g);
+        
+        Debug.Log("Spearmen:" +worldPosition);
+    }
+}
