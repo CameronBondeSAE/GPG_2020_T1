8169854c08efff9491d12cab241d8a2fb6f49664
Cam <c.bonde@sae.edu>
2020-02-20 12:41:55 +1100

    CamMonster death. Missile prefab that does damage

---
+++ Assets/Common/CamsRoomKeepOut/CamMonster.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMonster : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Subscribe to event
+        GetComponent<Health>().deathEvent += Death;
+    }
+
+    public void Death()
+    {
+        Destroy(gameObject);
+    }
+}
