e4d397214a9147036cb5010d5feadf286d8da18a
Cam <c.bonde@sae.edu>
2020-02-27 16:59:34 +1100

    no message

---
+++ Assets/Common/CamsRoomKeepOut/MusicManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public GameManager gameManager;
+    public AudioClip music;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager.startGameEvent += GameManagerOnstartGameEvent;
+    }
+
+    private void GameManagerOnstartGameEvent()
+    {
+        GetComponent<AudioSource>().clip = music;
+        GetComponent<AudioSource>().Play();
+    }
+
+}
