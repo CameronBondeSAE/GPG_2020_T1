94f809994abccd132f302929eef5038e2e9b3d64
Blaide Fedorowytsch <1017007@student.sae.edu.au>
2020-02-27 15:51:48 +1100

    playerBase NetworkBehaviour

--- Assets/GPG220/Blaide Fedorowytsch/Scripts/PlayerBase.cs
+++ Assets/GPG220/Blaide Fedorowytsch/Scripts/PlayerBase.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
 using GPG220.Luca.Scripts.Unit;
+using Mirror;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace GPG220.Blaide_Fedorowytsch.Scripts
 {
-    public class PlayerBase
+    public class PlayerBase : NetworkBehaviour
     {
         public List<UnitBase> units;
         public Color playerColour;
