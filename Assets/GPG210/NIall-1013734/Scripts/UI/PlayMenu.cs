428c543fbc6d1de63605b0c31071752efae4c942
Niall <1013734@student.sae.edu.au>
2020-02-27 11:43:52 +1100

    Play Menu Push

---
+++ Assets/GPG210/NIall-1013734/Scripts/UI/PlayMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayMenu : MonoBehaviour
+{
+
+    public GameObject PlayMenuUI;
+    
+    public void Awake()
+    {
+        PlayMenuUI.SetActive(true);
+    }
+
+
+    public void Play()
+    {
+       PlayMenuUI.SetActive(false);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
