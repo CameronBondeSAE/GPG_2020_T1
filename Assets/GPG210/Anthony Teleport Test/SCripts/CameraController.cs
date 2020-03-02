debefdb9fac3ebf7e600792b3a08dc1ed8c55d74
Anthony Younes <1016744@student.sae.edu.au>
2020-03-02 16:29:07 +1100

    Added a camera controller script to the main screen

---
+++ Assets/GPG210/Anthony Teleport Test/SCripts/CameraController.cs
@@ -0,0 +1,48 @@
+﻿
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panSpeed = 20f; //camera movement speed
+    public float panBorderThickness = 10f;
+    public Vector2 panLimit;
+    
+    public float scrollSpeed = 20;
+    public float minY = 20f;
+    public float maxY = 120f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness) //panning up
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness) //panning down
+        {
+            pos.z -= panSpeed * Time.deltaTime; 
+        }
+        
+        if (Input.GetKey("d") || Input.mousePosition.x >=Screen.width - panBorderThickness) //panning right
+        {
+            pos.x += panSpeed * Time.deltaTime; 
+        }
+       
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness) //panning left
+        {
+            pos.x -= panSpeed * Time.deltaTime; 
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y -= scroll * scrollSpeed * 100f * Time.deltaTime;
+       
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x); //clamps allows a set min and max value
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
+        
+        transform.position = pos; //getting the new position of the camera
+    }
+}
