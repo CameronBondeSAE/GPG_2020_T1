1d49b341bead2ad12fd2862c05ce90c19d373ae6
Blaide <Fedorowytsch>
2020-03-04 16:56:32 +1100

    Moved editor scripts > Editor folder.test Unit uses Abilities controller

--- Assets/GPG220/Blaide Fedorowytsch/Scripts/UnitTest.cs
+++ Assets/GPG220/Blaide Fedorowytsch/Scripts/UnitTest.cs
@@ -17,6 +17,7 @@ namespace GPG220.Blaide_Fedorowytsch.Scripts
         {
             Initialize();
             usm = FindObjectOfType<UnitSelectionManager>();
+           // health.deathEvent += Die(health);
         }
 
         public bool moving = false;
@@ -24,13 +25,12 @@ namespace GPG220.Blaide_Fedorowytsch.Scripts
         public List<ISelectable> selctionGroup;
         public float HeightOffset;
         public UnitSelectionManager usm;
-        
+
         public override void OnExecuteAction(Vector3 worldPosition, GameObject g)
         {
-            moving = true;
-            target = worldPosition;
+            abilityController.ExecuteDefaultAbility();
         }
-
+        
         void Update()
         {
 
@@ -38,56 +38,30 @@ namespace GPG220.Blaide_Fedorowytsch.Scripts
             {
                 RpcMove(transform.position);
             }
-            
-            
         }
+        
+
+        void Die( Health h)
+        {
+            Destroy(gameObject);
+        }
+
 
         [ClientRpc]
         public void RpcMove(Vector3 pos)
         {
-            
-            
             transform.position = pos;
         }
-
-        public override void OnSelected()
-        {
-            selctionGroup = usm.selectedIselectables;
-        }
+        
 
 
-        void Move(Vector3 v)
-        {
-            this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, v, 0.5f);
-        }
-
-        Vector3 OffsetPosition()
-        {
-            Vector3 total = new Vector3();
-
-            foreach (ISelectable s in selctionGroup)
-            {
-                total += ((MonoBehaviour) s).gameObject.transform.position;
-            }
-
-            return transform.position - total / selctionGroup.Count;
-        }
 
 
-        void FixedUpdate()
-        {
-            if (moving)
-            {
-                if (Vector3.Distance(this.gameObject.transform.position, target  + (Vector3.up *HeightOffset)  + OffsetPosition()) > 0.1f)
-                {
-                    Move(target + (Vector3.up *HeightOffset) + OffsetPosition());
-                }
-                else
-                {
-                    moving = false;
-                }
-            }
-        }
+
+
+
+
+
 
     }
 }
