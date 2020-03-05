27560f99ee593ba67fffcc4aad8a336547dfb79d
Blaide Fedorowytsch <1017007@student.sae.edu.au>
2020-03-05 11:29:30 +1100

    LocalPlayer, UnitSpawner has owner.

---
+++ Assets/GPG220/Blaide Fedorowytsch/Scripts/Player.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GPG220.Blaide_Fedorowytsch.Scripts;
+using UnityEngine;
+
+public class LocalPlayer : PlayerBase
+{
+    // Start is called before the first frame update
+}
