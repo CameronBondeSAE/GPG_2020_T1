db6663b395446f8249759edf67978464a8c3d979
Stephen <1013573@student.sae.edu.au>
2020-03-05 12:27:14 +1100

    RTS Manger added

---
+++ Assets/GPG230/RTSNetworkManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using GPG220.Blaide_Fedorowytsch.Scripts;
+using Mirror;
+using UnityEngine;
+
+public class RTSNetworkManager : NetworkManager
+{
+
+    public event Action <NetworkConnection> OnClientConnected;
+
+    public PlayerBase Player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnServerConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+        
+        OnClientConnected.Invoke(conn);
+        
+    }
+
+
+}
