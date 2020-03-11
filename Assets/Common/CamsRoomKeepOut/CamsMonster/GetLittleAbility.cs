64bb49bfddadd7d933bc09de5e558ef9022bc36d
Cam <c.bonde@sae.edu>
2020-03-12 10:36:35 +1100

    Added prefabs to main scene Added abilites to CamMonster

---
+++ Assets/Common/CamsRoomKeepOut/CamsMonster/GetLittleAbility.cs
@@ -0,0 +1,12 @@
+﻿using GPG220.Luca.Scripts.Abilities;
+using UnityEngine;
+
+public class GetLittleAbility : AbilityBase
+{
+    public override bool Execute(GameObject executorGameObject, GameObject[] targets = null)
+    {
+        Debug.Log("LITTLE!");
+
+        return true;
+    }
+}
