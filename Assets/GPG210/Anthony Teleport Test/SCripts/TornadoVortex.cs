71e72e40c436714dcde46656c1cfae1506cb691b
Anthony Younes <1016744@student.sae.edu.au>
2020-03-19 15:59:31 +1100

    Implemented new tornado physics & added tornado shader from assest store

--- Assets/GPG210/Anthony Teleport Test/SCripts/TornadoVortex.cs
+++ Assets/GPG210/Anthony Teleport Test/SCripts/TornadoVortex.cs
@@ -6,25 +6,66 @@ using UnityEngine;
 
 public class TornadoVortex : MonoBehaviour
 {
-   private GameObject pullObj;
-   public float pullSpeed;
-   public float rotateSpeed;
-   
-   //Ontrigger function called every frame for every collider other that is touching the trigger
-   public void OnTriggerStay(Collider other)
-   {
-      if (other.gameObject.GetComponent<Rigidbody>())
+    public Transform tornadoCenter;
+    public float pullSpeed;
+    public float refreshRate;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pullable")
+        {
+            StartCoroutine(PullObject(other, true)); //pull it in if pullable
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Pullable")
+        {
+            StartCoroutine(PullObject(other, false)); //dont pull it in if not pullable
+        }
+     
+    }
+
+    IEnumerator PullObject(Collider other, bool shouldPull)
+    {
+        if (shouldPull)
+        {
+            Vector3 ForceDir = tornadoCenter.position - other.transform.position;
+            other.GetComponent<Rigidbody>().AddForce(ForceDir.normalized * pullSpeed * Time.deltaTime);
+            yield return refreshRate;
+            StartCoroutine(PullObject(other, shouldPull)); //keeps checking if the object is in the tornado
+        }
+
+        
+    }
+
+    #region First TornadoPhysics Code
+    /*
+      private GameObject pullObj;
+      public float pullSpeed;
+      public float rotateSpeed;
+      
+      //Ontrigger function called every frame for every collider other that is touching the trigger
+      public void OnTriggerStay(Collider other)
       {
-         pullObj = other.gameObject;
-         
-         //if the object is pullable move it towards the tornado.
-         pullObj.GetComponent<Rigidbody>()
-            .AddForce(
-               Vector3.MoveTowards(pullObj.transform.position, this.transform.position, pullSpeed * Time.deltaTime),
-               ForceMode.Acceleration); 
+         if (other.gameObject.GetComponent<Rigidbody>())
+         {
+            pullObj = other.gameObject;
             
-         pullObj.transform.RotateAround(Vector3.zero, Vector3.up,rotateSpeed * Time.deltaTime);
-         
-      }
-   }
+            //if the object is pullable move it towards the tornado.
+            pullObj.GetComponent<Rigidbody>()
+               .AddForce(
+                  Vector3.MoveTowards(pullObj.transform.position, this.transform.position, pullSpeed * Time.deltaTime),
+                  ForceMode.Acceleration); 
+               
+            pullObj.transform.RotateAround(Vector3.zero, Vector3.up,rotateSpeed * Time.deltaTime);
+            
+         }
+      }*/
+   
+
+    #endregion
+  
+
 }
