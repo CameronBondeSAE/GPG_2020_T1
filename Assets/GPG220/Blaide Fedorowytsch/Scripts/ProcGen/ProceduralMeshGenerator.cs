b17521fb8b1a0d1c60cbbc550781cabda39f0a4e
Blaide <1017007@student.sae.edu.au>
2020-03-27 00:09:14 +1100

    Procedural Obstacles and Map generation

---
+++ Assets/GPG220/Blaide Fedorowytsch/Scripts/ProcGen/ProceduralMeshGenerator.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace GPG220.Blaide_Fedorowytsch.Scripts.ProcGen
+{
+    public class ProceduralMeshGenerator : MonoBehaviour
+    {
+        public bool showVerts = false;
+        private Vector3[] verticies;
+        private int[] triangles;
+        private Mesh mesh;
+        public Vector2Int meshResolution = new Vector2Int(20,20);
+        public Vector2 worldSize = new Vector2Int(20,20);
+        public Vector2 seed = new Vector2(0.3145f,0.1232f);
+        public float MeshConstructionDelay = 0.0001f;
+        public float heightMultiplier;
+        public float bumpiness;
+
+        public bool DrawGizmos = false;
+    
+        private void Start()
+        {
+            mesh = new Mesh();
+            mesh.name = "generatedTerrain";
+            GetComponent<MeshFilter>().mesh = mesh;
+            StartCoroutine(generateMesh());
+        }
+
+        private void Update()
+        {
+            UpdateMesh();
+        }
+        [Button(Name = "Regenerate Mesh")]
+        void RegenerateMesh()
+        {
+            StartCoroutine(generateMesh());
+        }
+
+        IEnumerator generateMesh()
+        {
+            verticies = new Vector3[(meshResolution.x + 1) * (meshResolution.y + 1)];
+
+        
+            for (int i = 0, y = 0; y <= meshResolution.y; y++)
+            {
+                for (int x = 0; x <= meshResolution.x; x++)
+                {
+                    float vertHeight = Mathf.PerlinNoise((seed.x + x) * bumpiness, (seed.y + y)* bumpiness) * heightMultiplier;
+                
+                    verticies[i] = new Vector3(x*((worldSize.x)/meshResolution.x),vertHeight,y * ((worldSize.y)/meshResolution.y));
+                    i++;
+                }
+            }
+
+            int vert = 0;
+            int tris = 0;
+            triangles = new int[meshResolution.x * meshResolution.y *6];
+            for (int y = 0; y < meshResolution.y; y++)
+            {
+                for (int x = 0; x < meshResolution.x; x++)
+                {
+                    triangles[tris + 0] =  vert + 0;
+                    triangles[tris + 1] =  vert + meshResolution.x + 1;
+                    triangles[tris + 2] =  vert + 1;
+                    triangles[tris + 3] =  vert + 1;
+                    triangles[tris + 4] =  vert + meshResolution.x + 1;
+                    triangles[tris + 5] =  vert + meshResolution.x + 2;
+                    vert++;
+                    tris += 6;
+                }
+                vert++;
+                yield return  new WaitForSeconds(MeshConstructionDelay);
+            }
+        
+        }
+
+        public float GetHeightAtPosition(Vector2 v)
+        {
+            return Mathf.PerlinNoise((seed.x + v.x) * bumpiness, (seed.y + v.y)* bumpiness) * heightMultiplier;
+        }
+    
+        [Button (Name = "test GetHeightAtPosition", Style = ButtonStyle.FoldoutButton)]
+        void TestHeightAtPosition(Vector2 v)
+        {
+            Debug.Log("Height At position: " + v + " = " + GetHeightAtPosition(v));
+        }
+
+        private void UpdateMesh()
+        {
+            mesh.Clear();
+            mesh.vertices = verticies;
+            mesh.triangles = triangles;
+            mesh.RecalculateNormals();
+        
+            Vector2[] uvs = new Vector2[verticies.Length];
+            int j = 0;
+            while (j < uvs.Length) {
+                uvs[j] = new Vector2(verticies[j].z/meshResolution.y, verticies[j].x / meshResolution.x);
+                j++;
+            }
+            mesh.uv = uvs;
+
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (verticies != null && showVerts && DrawGizmos)
+            {
+                for (int i = 0; i < verticies.Length; i++)
+                {
+                    Gizmos.color = Color.red;
+                    Gizmos.DrawSphere(verticies[i], 0.1f);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/GPG220/Blaide Fedorowytsch/Scripts/ProcGen/ProceduralObstacleSystem.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GPG220.Blaide_Fedorowytsch.Scripts.ProcGen
+{
+    public class ProceduralObstacleSystem : MonoBehaviour
+    {
+        [HideInInspector]
+        public bool[,] obstacleBoolGrid;
+        [HideInInspector]
+        public bool[,] lastObstacleBoolGrid;
+        [HideInInspector]
+        public GameObject[,] obstacleObjectGrid;
+
+        
+        
+        public Vector2Int gridSize = new Vector2Int(1,1);
+        public Vector2 worldSize;
+
+        private Vector2Int gridSizeLocker;
+
+        public float obstacleHeight;
+        public Vector2 obstacleGenerationDensity;
+        public Vector2 seed;
+        [Range(0,1)]
+        public float obstacleThreshold;
+
+        public GameObject obstaclePrefab;
+        public GameObject ObstacleHolder;
+
+        public bool DrawGizmoCubes = false;
+        // Start is called before the first frame update
+        void Start()
+        {
+            gridSizeLocker = gridSize;
+        GenerateBoolGrid();
+        GenerateObjectGrid();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            gridSize = gridSizeLocker;
+        UpdateGrids();
+        }
+        
+        void GenerateBoolGrid()
+        {
+            obstacleBoolGrid = new bool[gridSize.x,gridSize.y];
+            lastObstacleBoolGrid = new bool[gridSize.x,gridSize.y];
+            
+            for (int x = 0; x < gridSize.x; x++)
+            {
+                for (int y = 0; y < gridSize.y; y++)
+                {
+                    if (PerlinThresholdCheck(obstacleThreshold, obstacleGenerationDensity, new Vector2Int(x, y)))
+                    {
+                        obstacleBoolGrid[x, y] = true;
+
+                    }
+                    else
+                    {
+                        obstacleBoolGrid[x, y] = false;
+                    }
+                }   
+            }
+        }
+
+        void GenerateObjectGrid()
+        {
+            obstacleObjectGrid = new GameObject[gridSize.x, gridSize.y];
+            for (int x = 0; x < gridSize.x; x++)
+            {
+                for (int y = 0; y < gridSize.y; y++)
+                {
+                    obstacleObjectGrid[x, y] = Instantiate(obstaclePrefab,new Vector3((x) *0.01f* (worldSize.x/(gridSize.x*0.01f)), obstacleHeight/2, (y) *0.01f* (worldSize.y/(gridSize.y*0.01f))),transform.rotation);
+                    obstacleObjectGrid[x, y].transform.localScale = new Vector3(worldSize.x*(gridSize.x/100)*0.01f,obstacleHeight,worldSize.y* (gridSize.y/100)*0.01f);
+
+
+                    if (ObstacleHolder)
+                    {
+                        obstacleObjectGrid[x, y].transform.parent = ObstacleHolder.transform;
+                    }
+                    else
+                    {
+                       obstacleObjectGrid[x, y].transform.parent = transform; 
+                    }
+
+                    
+                    
+                    
+                    if (PerlinThresholdCheck(obstacleThreshold, obstacleGenerationDensity, new Vector2Int(x, y)))
+                    {
+                        obstacleObjectGrid[x,y].SetActive(true);
+                    }
+                    else
+                    {
+                        obstacleObjectGrid[x,y].SetActive(false);
+                    }
+                }   
+            }
+        }
+
+
+        List<Vector2Int> CompareBoolGrids()
+        {
+            List<Vector2Int> list = new List<Vector2Int>();
+            for (int x = 0; x < gridSize.x; x++)
+            {
+                for (int y = 0; y < gridSize.y; y++)
+                {
+                    if (obstacleBoolGrid[x,y] != lastObstacleBoolGrid[x,y])
+                    {
+                        list.Add(new Vector2Int(x,y));
+                    }
+                }
+            }
+            return list;
+        }
+
+        public void UpdateGrids()
+        {
+            for (int x = 0; x < gridSize.x; x++)
+            {
+                for (int y = 0; y < gridSize.y; y++)
+                {
+                    if (PerlinThresholdCheck(obstacleThreshold, obstacleGenerationDensity, new Vector2Int(x, y)))
+                    {
+                        obstacleBoolGrid[x, y] = true;
+                    }
+                    else
+                    {
+                        obstacleBoolGrid[x, y] = false;
+                    }
+                }  
+            }
+
+            foreach (Vector2Int gridPosition in CompareBoolGrids())
+            {
+                if (obstacleBoolGrid[gridPosition.x, gridPosition.y])
+                {
+                    obstacleObjectGrid[gridPosition.x,gridPosition.y].SetActive(true); 
+                }
+                else
+                {
+                    obstacleObjectGrid[gridPosition.x,gridPosition.y].SetActive(false);
+                }
+
+                lastObstacleBoolGrid[gridPosition.x, gridPosition.y] = obstacleBoolGrid[gridPosition.x, gridPosition.y];
+            }
+
+
+        }
+
+        public bool PerlinThresholdCheck(float threshold, Vector2 density, Vector2Int gridPosition)
+        {
+            if (Mathf.PerlinNoise((gridPosition.x + seed.x )* density.x, (gridPosition.y + seed.y) * density.y ) >= threshold)
+            {
+                return true; 
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (DrawGizmoCubes)
+            {
+                //GenerateGrid();
+                for (int x = 0; x < gridSize.x; x++)
+                {
+                    for (int y = 0; y < gridSize.y; y++)
+                    {
+                        if (obstacleBoolGrid[x,y])
+                        {
+                            Gizmos.DrawCube(new Vector3((x) *0.01f* (worldSize.x/(gridSize.x*0.01f)), obstacleHeight/2, (y) *0.01f* (worldSize.y/(gridSize.y*0.01f))), new Vector3(worldSize.x*(gridSize.x/100)*0.01f,obstacleHeight,worldSize.y* (gridSize.y/100)*0.01f));
+                        }
+                    }
+                }
+            }
+            
+
+        }
+    }
+    
+}
