2205b0bf504bfa6c61f12223dea4989094710eac
Cameron Bonde <c.bonde@sae.edu>
2020-03-26 12:06:41 +1100

    Ability UI is back

---
+++ Assets/Common/CamsRoomKeepOut/Scripts/TestNetworkBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestNetworkBehaviour : MonoBehaviour
+{
+	public Light light;
+
+	public CamMonster camMonster;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		camMonster = FindObjectOfType<CamMonster>();
+
+		light = FindObjectOfType<Light>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+}
