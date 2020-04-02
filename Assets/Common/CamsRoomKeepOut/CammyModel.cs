9a36d4bfe21e30c7b4fa00a882c6f52c1e37eb67
Cameron Bonde <c.bonde@sae.edu>
2020-04-02 13:12:27 +1100

    Main prefabs

---
+++ Assets/Common/CamsRoomKeepOut/CammyModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CammyModel : MonoBehaviour
+{
+	public Animator animator;
+	public Health health;
+
+	private void Start()
+	{
+		health.deathEvent += HealthOndeathEvent;
+	}
+
+	private void HealthOndeathEvent(Health obj)
+	{
+		animator.SetTrigger("Die");
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		animator.SetTrigger("Something hit me");
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		animator.SetTrigger("Something left my trigger");
+	}
+}
---
+++ Assets/Common/CamsRoomKeepOut/SpinState.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror.Examples.Tanks;
+using Mono.CecilX.Cil;
+using UnityEngine;
+
+public class SpinState : StateMachineBehaviour
+{
+	public float speed;
+	public ParticleSystem particleSystem;
+	
+	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		particleSystem.Play();
+	}
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+		animator.GetComponent<Transform>().Rotate(0, speed * Time.deltaTime, 0);
+	}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        particleSystem.Stop();
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
