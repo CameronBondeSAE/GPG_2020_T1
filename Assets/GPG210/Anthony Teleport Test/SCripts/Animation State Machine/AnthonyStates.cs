73eaa7d4ee3005a74d33d83708234ecdd416efcb
ayounes2001 <1016744@student.sae.edu.au>
2020-04-02 14:17:57 +1100

    My Animation State Machine Test

---
+++ Assets/GPG210/Anthony Teleport Test/SCripts/Animation State Machine/AnthonyStates.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnthonyStates : MonoBehaviour
+{
+    public Animator animator;
+
+    public Health health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health.deathEvent += HealthOndeathEvent;
+    }
+
+    private void HealthOndeathEvent(Health obj)
+    {
+        animator.SetTrigger("Player Death");
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetTrigger("Move player");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        animator.SetTrigger("I escaped");
+    }
+}
