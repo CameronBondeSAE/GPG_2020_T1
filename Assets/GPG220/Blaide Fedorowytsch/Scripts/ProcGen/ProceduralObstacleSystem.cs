c72abfe69932a91c854b9847bfd177a1d50f6525
Blaide <1017007@student.sae.edu.au>
2020-04-02 15:32:10 +1100

    Broken Growth Algorithm

--- Assets/GPG220/Blaide Fedorowytsch/Scripts/ProcGen/ProceduralObstacleSystem.cs
+++ Assets/GPG220/Blaide Fedorowytsch/Scripts/ProcGen/ProceduralObstacleSystem.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace GPG220.Blaide_Fedorowytsch.Scripts.ProcGen
 {
@@ -27,13 +29,26 @@ namespace GPG220.Blaide_Fedorowytsch.Scripts.ProcGen
         public float obstacleThreshold;
         public float obstacleThresholdLast;
 
+
+        public bool growObstacles;
+        
+        public float baseGrowRate;
+        public AnimationCurve growcurve;
+        public float roundTime;
+        public float timer;
+        
+
         public GameObject obstaclePrefab;
         public GameObject ObstacleHolder;
 
         public GameManager gameManager;
+        public bool applyOverrideGrid = false;
 
-        private bool gameStarted = false;
+        public List<Vector2Int> closedEdges;
+        public List<Vector2Int> openEdges;
         
+        private bool gameStarted = false;
+
         // Start is called before the first frame update
         void Start()
         {
@@ -52,22 +67,139 @@ namespace GPG220.Blaide_Fedorowytsch.Scripts.ProcGen
         // Update is called once per frame
         void Update()
         {
-            if (Math.Abs(obstacleThreshold - obstacleThresholdLast) > 0.001f)
+
+
+            if (Math.Abs(obstacleThreshold - obstacleThresholdLast) > 0.001f ) // only update boolGrids With Perlin if the threshold Changes.
             {
                 gridSize = gridSizeLocker; 
                 if (gameStarted) 
                 { 
-                    UpdateGrids();
+                    UpdateBoolGridsWithPerlin();
+                    UpdateObstacleGrid();
+                    FindEdgeObstacles();
                 }
                 obstacleThresholdLast = obstacleThreshold;
             }
+            
+            if (growObstacles)
+            {
+                if (timer < roundTime)
+                {
+                    timer += Time.deltaTime;
+                }
+                else
+                {
+                    timer = 0f;
+                    GrowObstacleBoolGrid();
+                    UpdateObstacleGrid();
+                }
+            }
+
+        }
+
+        private void FindEdgeObstacles()
+        {
+            openEdges.Clear();
+            closedEdges.Clear();
+            List<Vector2Int> edges = new List<Vector2Int>();
+            for (int x = 0; x < gridSize.x; x++)
+            {
+                for (int y = 0; y < gridSize.y; y++)
+                {
+                    if (obstacleBoolGrid[x,y])
+                    {
+                        foreach (Vector2Int gridposition in GetNeighbourPositions(new Vector2Int(x, y)))
+                        {
+                            if (obstacleBoolGrid[gridposition.x, gridposition.y] == false)
+                            {
+                                edges.Add(new Vector2Int(x, y));
+                                break;
+                            }
+                        }
+                    }
+                }   
+            }
+
+            closedEdges = edges;
+
+            foreach (Vector2Int gridPosition in closedEdges)
+            {
+                foreach (Vector2Int g in GetNeighbourPositions(gridPosition))
+                {
+                    if (obstacleBoolGrid[g.x, g.y] == false)
+                    {
+                        openEdges.Add(g);
+                    }
+                }
+            }
+            
+        }
+
+        public void GrowObstacleBoolGrid()
+        {
+            List<Vector2Int> temp = new List<Vector2Int>();
+            foreach (Vector2Int gridPosition in openEdges)
+            {
+                if (Random.Range(1,100) < 20 && obstacleBoolGrid[gridPosition.x, gridPosition.y] == false)
+                {
+                    temp.Add(gridPosition);
+                    obstacleBoolGrid[gridPosition.x, gridPosition.y] = true;
+                }
+            }
+
+            foreach (Vector2Int gridPosition in temp)
+            {
+                openEdges.Remove(gridPosition);
+                closedEdges.Add(gridPosition);
+                foreach (Vector2Int neighbourPosition in GetNeighbourPositions(gridPosition))
+                {
+                    if (obstacleBoolGrid[neighbourPosition.x, neighbourPosition.y] == false)
+                    {
+                        openEdges.Add(neighbourPosition);
+
+                    }
+                }
+            }
+        }
+
+        public List<Vector2Int> GetNeighbourPositions(Vector2Int gridPosition)
+        {
+            List<Vector2Int> neighbours = new List<Vector2Int>();
+
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    if (x == 0 && y == 0)
+                    {
+                        continue;
+                    }
+
+                    int checkX = gridPosition.x + x;
+                    int checkY = gridPosition.y + y;
+
+
+                    if (checkX >= 0 && checkX < gridSize.x && checkY >= 0 && checkY < gridSize.y)
+                    {
+                        neighbours.Add( new Vector2Int(checkX,checkY));
+                    }
+                }
+            }
+
+            return neighbours;
+
         }
         
+       
+        
+        
+
+
         void GenerateBoolGrid()
         {
             obstacleBoolGrid = new bool[gridSize.x,gridSize.y];
             lastObstacleBoolGrid = new bool[gridSize.x,gridSize.y];
-            
+
             for (int x = 0; x < gridSize.x; x++)
             {
                 for (int y = 0; y < gridSize.y; y++)
@@ -137,12 +269,13 @@ namespace GPG220.Blaide_Fedorowytsch.Scripts.ProcGen
             return list;
         }
 
-        public void UpdateGrids()
+        public void UpdateBoolGridsWithPerlin()
         {
             for (int x = 0; x < gridSize.x; x++)
             {
                 for (int y = 0; y < gridSize.y; y++)
                 {
+                    lastObstacleBoolGrid[x, y] = obstacleBoolGrid[x, y];
                     if (PerlinThresholdCheck(obstacleThreshold, obstacleGenerationDensity, new Vector2Int(x, y)))
                     {
                         obstacleBoolGrid[x, y] = true;
@@ -153,8 +286,14 @@ namespace GPG220.Blaide_Fedorowytsch.Scripts.ProcGen
                     }
                 }  
             }
+        }
+        
+        
+        void UpdateObstacleGrid()
+        {
+            List<Vector2Int> gridPositions = CompareBoolGrids() ;
 
-            foreach (Vector2Int gridPosition in CompareBoolGrids())
+            foreach (Vector2Int gridPosition in gridPositions)
             {
                 if (obstacleBoolGrid[gridPosition.x, gridPosition.y])
                 {
@@ -165,10 +304,8 @@ namespace GPG220.Blaide_Fedorowytsch.Scripts.ProcGen
                     obstacleObjectGrid[gridPosition.x,gridPosition.y].SetActive(false);
                 }
 
-                lastObstacleBoolGrid[gridPosition.x, gridPosition.y] = obstacleBoolGrid[gridPosition.x, gridPosition.y];
+                
             }
-
-
         }
 
         public bool PerlinThresholdCheck(float threshold, Vector2 density, Vector2Int gridPosition)
@@ -182,7 +319,15 @@ namespace GPG220.Blaide_Fedorowytsch.Scripts.ProcGen
                 return false;
             }
         }
-        
+
+        private void OnDrawGizmosSelected()
+        {
+            foreach (Vector2Int v in openEdges)
+            {
+                Gizmos.DrawSphere(transform.position + new Vector3(v.x*(worldSize.x*0.01f),8,v.y*(worldSize.y*0.01f)), 2f);
+            }
+        }
     }
+    
     
 }
