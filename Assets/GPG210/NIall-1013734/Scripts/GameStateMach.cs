cf0699af807dfdafdd2ad1c820f1d077825a2c3a
Niall <1013734@student.sae.edu.au>
2020-04-08 16:43:25 +1000

    Cube Tween + GameStateMach start

---
+++ Assets/GPG210/NIall-1013734/Scripts/GameStateMach.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateMach : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Start()
+    {
+        
+    }
+}
