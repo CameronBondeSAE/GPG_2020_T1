4bc2b57df5e64a85840a0ed5aa15a17facb98dec
Cameron Bonde <c.bonde@sae.edu>
2020-04-09 11:23:38 +1000

    no message

---
+++ Assets/Common/CamsRoomKeepOut/Tests/OdinSerializedUnityComponent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public interface ISomeInterface
+{
+	string SomeInterfaceProperty { get; set; }
+}
+
+public class OdinSerializedUnityComponent : MonoBehaviour
+{}
+	
+	
+/*	
+	
+	: SerializedMonoBehaviour, ISomeInterface
+{
+	[OdinSerialize]
+	public virtual string VirtualProperty { get; set; } // Wil not be shown or serialized, since it is virtual 
+    
+	[OdinSerialize]
+	public string SomeInterfaceProperty { get; set; } // Wil not be shown or serialized, since it is virtual because of the interface
+}*/
