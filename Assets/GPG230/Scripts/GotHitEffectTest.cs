ca5dced0848d3231f0ae3d78986e7adf1e2305e2
Stephen Greenland <1013573@student.sae.edu.au>
2020-04-09 17:44:44 +1000

    Shaders added and edited

---
+++ Assets/GPG230/Scripts/GotHitEffectTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GotHitEffectTest : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+    public float Amount;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        meshRenderer.material.SetFloat("_Amount", Amount);
+        
+        
+    }
+}
