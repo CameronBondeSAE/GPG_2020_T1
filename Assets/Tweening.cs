3c74677482f3a0b799c606d54131a236a9b5467c
Niall <1013734@student.sae.edu.au>
2020-04-15 16:31:43 +1000

    Obstacles Grow from floor up.
    viewmodel & collider seperated

--- Assets/Tweening.cs
+++ Assets/Tweening.cs
@@ -8,17 +8,18 @@ public class Tweening : MonoBehaviour
 {
     private void Start()
     {
-        transform.localScale = new Vector3(2f, 1f, 2f);
+        transform.localScale = new Vector3(1f, 0f, 1f);
     }
 
     public void OnEnable()
     {
-        transform.localScale = new Vector3(2f, 1f, 2f);
-        transform.DOScale(new Vector3(2f, 11.34f, 2f), 5f).SetEase(Ease.Linear);
+        transform.localScale = new Vector3(1f, 0f, 1f);
+        //  transform.DOScale(new Vector3(2f, 11.34f, 2f), 5f).SetEase(Ease.Linear);
+        transform.DOScale(new Vector3(1f, 1f, 1f), 5f).SetEase(Ease.Linear);
     }
 
     public void OnDisable()
     {
-        transform.localScale = new Vector3(2f, 1f, 2f);
+        transform.localScale = new Vector3(1f, 0f, 1f);
     }
 }
