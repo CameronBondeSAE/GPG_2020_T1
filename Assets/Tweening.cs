a82f8f3a402b5d2f459d5d5d01c47017226ac854
Niall <1013734@student.sae.edu.au>
2020-04-16 12:05:25 +1000

    Tron Mat added to Unit, Wall function rename.
    OnEnable > Appear
    OnDisable > Disappear

--- Assets/Tweening.cs
+++ Assets/Tweening.cs
@@ -6,20 +6,26 @@ using DG.Tweening;
 
 public class Tweening : MonoBehaviour
 {
+
+    public Collider collider;
+    
     private void Start()
     {
         transform.localScale = new Vector3(1f, 0f, 1f);
     }
 
-    public void OnEnable()
+    public void Appear()
     {
+        collider.enabled = true;
         transform.localScale = new Vector3(1f, 0f, 1f);
         //  transform.DOScale(new Vector3(2f, 11.34f, 2f), 5f).SetEase(Ease.Linear);
         transform.DOScale(new Vector3(1f, 1f, 1f), 5f).SetEase(Ease.Linear);
     }
 
-    public void OnDisable()
+    public void Disappear()
     {
-        transform.localScale = new Vector3(1f, 0f, 1f);
+        transform.localScale = new Vector3(1f, 1f, 1f);
+        transform.DOScale(new Vector3(1f, 0f, 1f), 5f).SetEase(Ease.Linear);
+        collider.enabled = false;
     }
 }
