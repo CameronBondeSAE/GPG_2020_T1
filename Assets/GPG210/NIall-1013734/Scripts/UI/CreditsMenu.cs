4ecd9c71aac339bad24dab1810401f3cf1d890f1
Niall <1013734@student.sae.edu.au>
2020-04-22 16:11:18 +1000

    UI Work
    +Readable Title
    +Options Menu
    +Credits Menu
    - Options button in Pause Menu

---
+++ Assets/GPG210/NIall-1013734/Scripts/UI/CreditsMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsMenu : MonoBehaviour
+{
+
+    public PlayMenu playMenu;
+    public GameObject creditsMenu;
+
+    public void Back()
+    {
+        playMenu.PlayMenuUI.SetActive(true);
+        creditsMenu.SetActive(false);
+    }
+}
---
+++ Assets/GPG210/NIall-1013734/Scripts/UI/OptionsMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+    public PlayMenu playmenu;
+    public CreditsMenu creditsMenu;
+    public GameObject optionsMenu;
+    
+
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void Credits()
+    {
+        creditsMenu.creditsMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+       
+    }
+
+    public void Back()
+    {
+        playmenu.PlayMenuUI.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+}
