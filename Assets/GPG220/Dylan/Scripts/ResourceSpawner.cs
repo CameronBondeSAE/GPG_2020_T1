b3ed683547e4623e6fe4c0705e70bff839b9db26
Dylan <dylanwebb5@hotmail.com>
2020-05-08 14:40:27 +1000

    Resource Spawner

---
+++ Assets/GPG220/Dylan/Scripts/ResourceSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using GPG220.Blaide_Fedorowytsch.Scripts.ProcGen;
+using Mirror;
+using UnityEngine;
+
+namespace GPG220.Dylan.Scripts
+{
+    public class ResourceSpawner : MonoBehaviour
+    {
+        public MapUtilities mapUtilities;
+        public ProceduralMeshGenerator proceduralMeshGenerator;
+        public GameManager gameManager;
+        public float amountToSpawn;
+        public GameObject resourcePrefab;
+
+        public void Awake()
+        {
+            gameManager = FindObjectOfType<GameManager>();
+            mapUtilities = FindObjectOfType<MapUtilities>();
+            proceduralMeshGenerator = FindObjectOfType<ProceduralMeshGenerator>();
+            gameManager.startGameEvent += gameManagerOnstartGameEvent;
+            // gameManager.GameOverEvent += gameManagerOnstartGameEvent;
+        }
+
+
+        public void SpawnResources(float amountToSpawn)
+        {
+            for (int i = 0; i < amountToSpawn; i++)
+            {
+                var tempResource = Instantiate(resourcePrefab,
+                    mapUtilities.RandomGroundPointInBounds(proceduralMeshGenerator.mesh.bounds, new Vector3(1, 1, 1)),
+                    Quaternion.identity);
+                NetworkServer.Spawn(tempResource);
+            }
+
+            // gameManager.startGameEvent -= gameManagerOnstartGameEvent;
+        }
+
+        void gameManagerOnstartGameEvent() => SpawnResources(amountToSpawn);
+    }
+}
