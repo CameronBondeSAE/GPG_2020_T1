d55610b87166b595588b46ec3813b293a79ffcf2
Blaide <1017007@student.sae.edu.au>
2020-05-08 18:02:29 +1000

    Gold UI Not Networked

---
+++ Assets/GPG220/Blaide Fedorowytsch/Scripts/UI/ResourceUI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GPG220.Luca.Scripts.Resources;
+using TMPro;
+using UnityEngine;
+
+public class ResourceUI : MonoBehaviour
+{
+    public Inventory kingInv;
+
+    public TextMeshProUGUI TMP;
+    public GameObject panel;
+    private GameManager gameManager;
+    public ResourceType resourceType;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        gameManager.StartGameEvent += gameStart;
+        panel.SetActive(false);
+    }
+    
+
+    public void gameStart()
+    {
+        StartCoroutine(DelayedKingInvCheck()); // Hack TODO: Maybe have an event invoked after a king is spawned.
+    }
+
+    private IEnumerator DelayedKingInvCheck()
+    {
+        yield return new WaitForSeconds(1f);
+        kingInv = gameManager.localPlayer.king.inventory;
+        kingInv.onResQuantityChanged += UpdateResCount;
+        TMP.text = "Gold : " + kingInv.GetResourceQuantity(resourceType);
+        panel.SetActive(true);
+    }
+
+    public void UpdateResCount(Inventory inventory, ResourceType _resourceType, int amtChange)
+    {
+        if (_resourceType == resourceType)
+        {
+            TMP.text = "Gold : " + inventory.GetResourceQuantity(resourceType);
+        }
+    }
+
+}
+
+
