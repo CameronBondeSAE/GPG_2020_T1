51696e954631f25db107116b14ff97f26e3e5060
Cameron Bonde <c.bonde@sae.edu>
2020-05-11 16:59:22 +1000

    Healthbar spawning/target

--- Assets/GPG210/Anthony Teleport Test/SCripts/GameManager.cs
+++ Assets/GPG210/Anthony Teleport Test/SCripts/GameManager.cs
@@ -102,6 +102,7 @@ public class GameManager : MonoBehaviour
 			if (healthbarPrefab != null)
 			{
 				HealthbarViewModel healthBar = Instantiate(healthbarPrefab);
+				healthBar.target = playerBase.transform;
 			}
 		}
 	}
--- Assets/GPG210/NIall-1013734/Scripts/UI/HealthbarViewModel.cs
+++ Assets/GPG210/NIall-1013734/Scripts/UI/HealthbarViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,15 +8,22 @@ public class HealthbarViewModel : MonoBehaviour
 
     public HealthBar healthbar;
     public Health health;
-    
+	public Transform target;
+	public Vector3 offset;
 
-    private void Awake()
+
+	private void Awake()
     {
         healthbar.SetMaxHealth(health.startingHealth);
         health.healthChangedEvent += HealthOnhealthChangedEvent;
     }
 
-    private void HealthOnhealthChangedEvent(Health arg1, int arg2)
+	private void Update()
+	{
+		transform.position = target.position + offset;
+	}
+
+	private void HealthOnhealthChangedEvent(Health arg1, int arg2)
     {
          healthbar.SetHealth(health.CurrentHealth);
     }
